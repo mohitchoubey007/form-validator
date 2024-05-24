f56b8ede52d7352380cda7fcd2f278dd760d262c
Mohit Choubey <mohitjdh2021@gmail.com>
2024-05-24 16:19:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FormComponent from './component/formcomponent.jsx'
+function App() {
+ 
+
+  return (
+    <>
+    <FormComponent></FormComponent>
+    </>
+  )
+}
+
+export default App
